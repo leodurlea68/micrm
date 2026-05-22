2cac4771f8ab07550a29d79c72e8fa0b43a72a77
Leonardo Durán <leonardo.duran@intelligy.com>
2026-05-22 14:57:23 -0600

    Fix: Eliminado el error crm_prpject de todos los archivos

--- crm_project/urls.py
+++ crm_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for crm_prpject project.
+URL configuration for crm_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
