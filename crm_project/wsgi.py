b5b5fc088b096d8d8aa5401b08bfae8415665510
Leonardo Durán <leonardo.duran@intelligy.com>
2026-05-25 12:47:05 -0600

    Fix: Mover migraciones después de configurar Django

--- crm_project/wsgi.py
+++ crm_project/wsgi.py
@@ -8,14 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_project.settings')
+
+# Forzar migraciones DESPUÉS de configurar Django
+application = get_wsgi_application()
+
+from django.core.management import call_command
 try:
     call_command('migrate', interactive=False)
 except Exception as e:
-    print(f"Error al migrar: {e}")
-    
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_project.settings')
-
-application = get_wsgi_application()
+    print(f"Error al migrar (no crítico): {e}")
